cc2e464a83c400f1ce0c740306e02946825cf712
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-03-21 17:33:37 +0000

    commit connect Mongoose

--- API/config/config.js
+++ API/config/config.js
@@ -1,9 +1,9 @@
 // check env.
-let env = process.env.NODE_ENV || 'development'
+var env = process.env.NODE_ENV || 'development'
 
 // fetch env. config
-let config = require('./config.json');
-let envConvig = config[env];
+var config = require('./config.json');
+var envConvig = config[env];
 
 //add env. config values to process.env
 Object.keys(envConvig).forEach(key => process.env[key] = envConvig[key]);
