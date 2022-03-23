6d83c6c4e78e64d018aed9e99658bbff5e61b801
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-03-23 16:18:46 +0000

    FrontEnd user-sign_up

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/src/app/shared/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { User } from './user.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  selectedUser: User = {
+    fullName: '',
+    email: '',
+    password: ''
+  };
+
+  constructor() { }
+}
