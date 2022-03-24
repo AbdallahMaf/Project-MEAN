d260e1e35fe48f9ec4e3c7e63edb7e1b4a7bbe9b
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-03-24 18:26:15 +0000

    contrainte sign-up

--- Angular/src/app/user/sign-up/sign-up.component.ts
+++ Angular/src/app/user/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PatternValidator, RequiredValidator } from '@angular/forms';
 import { UserService } from '../../shared/user.service';
+
 
 
 
@@ -11,6 +12,11 @@ import { UserService } from '../../shared/user.service';
   providers : [UserService]
 })
 export class SignUpComponent implements OnInit {
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+
+
 
   constructor(public userService: UserService) { }
 
