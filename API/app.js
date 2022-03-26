4db31c058c0d424f91b1ccce52ae255765b72afa
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-03-26 13:23:27 +0000

    API Rectificate error app.js

--- API/app.js
+++ API/app.js
@@ -14,7 +14,7 @@ const rtsIndex = require('./routes/index.router');
 const app = express();
 
 //middleware
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(cors());
 app.use(passport.initialize());
 app.use('/api', rtsIndex)
