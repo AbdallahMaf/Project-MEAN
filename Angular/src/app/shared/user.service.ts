4b3ad8c0e2da04e77ef26c57c4e93c956fe351b1
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-03-28 03:36:29 +0000

    Secure Private Route1

--- Angular/src/app/shared/user.service.ts
+++ Angular/src/app/shared/user.service.ts
@@ -27,4 +27,26 @@ export class UserService {
     setToken(token: string) {
       localStorage.setItem('token', token)
     }
+
+    deleteToken() {
+      localStorage.removeItem('token');
+    }
+
+    getUserPayload() {
+      var token = localStorage.getItem('token');
+      if (token) {
+        var userPayload = atob(token.split('.')[1]);
+        return JSON.parse(userPayload);
+      }
+      else
+        return null;
+    }
+
+    isLoggedIn() {
+      var userPayload = this.getUserPayload();
+      if (userPayload)
+        return userPayload.exp > Date.now() / 1000;
+      else
+        return false;
+    }
 }
